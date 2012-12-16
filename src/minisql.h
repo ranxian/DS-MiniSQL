84b24ffbf74942af3407fc1c45103fdaade10769
鲜染 Th0ar <xranthoar@gmail.com>
2012-12-16 22:13:06 +0800

    definition.h

--- src/minisql.h
+++ src/minisql.h
@@ -11,7 +11,7 @@
 #include <cstdlib>
 #include <fstream>
 
-/* 常用宏定义 */
+/* 全局配置 */
 #define MAX_FILENAME_LENGTH 256 /* 最长文件名长度 */
 #define MAX_CMD_LENGTH      256 /* 最长命令长度 */
 #define MAX_ATTRNAME_LENGTH 256 /* 最长字段长度 */
@@ -19,4 +19,8 @@
 #define MAX_ATTR_NUM        32  /* 一个表的最长字段数目 */
 #define TABLE_LIST "table.list" /* 保存表信息的文件 */
 #define INDEX_LIST "index.list" /* 保存索引信息的文件 */
+#define FILE_PAGESIZE 4096      /* 虚拟页大小 */
+#define MEM_MAXPAGE 1000        /* 内存页最大数目 */
+#define FILENAME_MAXLEN 256     /* 文件名最大长度 */
+
 #endif
