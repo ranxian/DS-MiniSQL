36c975a3078a6f34f24d7d47090b75a6f74e9f24
鲜染 Th0ar <xranthoar@gmail.com>
2012-12-16 20:46:15 +0800

    .

--- src/minisql.cpp
+++ src/minisql.cpp
@@ -1,20 +1,5 @@
 #include "minisql.h"
 
-void Init();
-void Run();
-void Exit();
-void createTable(Information_t *info);
-void createIndex(Information_t *info);
-void createDatabse(Information_t *info);
-void select(Information_t *info);
-void insert(Information_t *info);
-void dropTable(Information_t *info);
-void dropIndex(Information_t *info);
-void dropDatabase(Information_t *info);
-void deleteRecord(Information_t *info);
-void execfiles(Information_t *info);
-
-
 int main()
 {
     Init();             // 初始化一些参数
--- src/minisql.h
+++ src/minisql.h
@@ -3,3 +3,17 @@
 #include <iostream>
 using namespace std;
 Catalog catlog_manager;
+
+void Init();
+void Run();
+void Exit();
+void createTable(Information_t *info);
+void createIndex(Information_t *info);
+void createDatabse(Information_t *info);
+void select(Information_t *info);
+void insert(Information_t *info);
+void dropTable(Information_t *info);
+void dropIndex(Information_t *info);
+void dropDatabase(Information_t *info);
+void deleteRecord(Information_t *info);
+void execfiles(Information_t *info);
