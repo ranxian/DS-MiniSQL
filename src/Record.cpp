aee324640dcc0a78554c305d85fd81159e3c2179
full fool <cyq_2210@163.com>
2012-12-19 22:05:59 +0800

    写了一些Record的代码

--- src/Record.cpp
+++ src/Record.cpp
@@ -1,15 +1,13 @@
 /*
 Record.cpp
-
-未知：
-lastInsert
-totalLenth
+print和printhead分别干嘛
+select从何搞起
+如何输入(所有空的地方输入"oop",占满字节数)
 */
 
-#include<iostream>
-#include<string>
-#include<fstream>
-#include "minisql.h"
+
+
+#include "Record.h"
 
 
 /* 所有的文件的名字是表的名字后面加后缀.rec */
@@ -18,41 +16,99 @@ totalLenth
 int Record::Insert(info_t & insert_info)
 {
     /* 根据表明生成文件名 */
-    char filename[40];
+    string Filename;
+    Filename = "\"" + insert_info.tableName + ".rec\"";
+
+    //filename即为表名处理过的字符串
+    char *filename = (char *)Filename.c_str();                  
     int i;
-    int tableLength = insert_info.t.name.length();
-    filename[0] = '"';
-    for (i=1; i<=tableLength; i++)
-    {
-        filename[i] = insert_info.t.name[i-1];
-    }
-    filename[tableLength + 1] = '.';
-    filename[tableLength + 2] = 'r';
-    filename[tableLength + 3] = 'e';
-    filename[tableLength + 4] = 'c';
-    filename[tableLength + 5] = '"';
-    filename[tableLength + 6] = '\0';
+
     ofstream output;
-    output.open(filename,ios::app|ios::binary|ios::out);
+    output.open(filename, ios::app|ios::binary|ios::out);
     map<string, string> ::iterator iter;
-    for (iter=insert_info.insertItems)
+    //对这个表中每个字段进行遍历，看是否在map对中某个字段的数据
+    for (i=0; i<insert_info.t.attrNumber; i++)                  
+    {                                                         
+        attr_t target = insert_info.t.attributes[i];
+        for (iter=insert_info.insertItems.begin(); iter!=insert_info.insertItems.end();
+            iter++)
+        {
+            //如果某个字段在map中存在
+            if (target.name == iter->first)                      
+            {
+                 //字段的值（string类型）
+                string temp = iter->second;                     
+                output.write((char *)temp.c_str(), target.length);
+                break;
+            }
+        }
+        if (iter != insert_info.insertItems.end())               
+            continue;
+        
+        //如果某个字段不在map中，则输入空的字节数
+        else                                                     
+        {
+            string hollow = "oop";
+            output.write((char *)hollow.c_str(), target.length);
+        }
 
-    
 
-
-
-
+    }
 
 }
 
-void Record::Delete(info_t & delete_info, index_node_t & index)
+/* 将所有的字段值都改为oop */
+void Record::Delete(info_t & delete_info, index_node_t & index)         
 {
-
+    string Filename;
+    Filename = "\"" + delete_info.tableName + ".rec\"";
+    char *filename = (char *)Filename.c_str();  
+    ofstream output;
+    output.open(filename, ios::binary|ios::app|ios::out);
+    //定位到相应记录
+    output.seekp(index.offset, ios::beg);                     
+    table_t target = delete_info.t;
+    int i;
+    string temp = "oop";
+     //对所有字段都写入oop串
+    for (i=0; i<target.attrNumber; i++)                      
+    {
+        output.write((char *)temp.c_str(), target.attributes[i].length);
+    }
 }
 
+
+/* 基本处理同insert */
 void Record::Update(info_t & update_info, index_node_t & index)
 {
+    string Filename;
+    Filename = "\"" + update_info.tableName + ".rec\"";
+    char *filename = (char *)Filename.c_str();  
+    ofstream output;
+    output.open(filename, ios::binary|ios::app|ios::out);
 
+     //定位到相应的记录
+    output.seekp(index.offset, ios::beg);                      
+    map<string, string> ::iterator iter;
+    int i;
+    for (i=0; i<update_info.t.attrNumber; i++)                  
+    {                                                           
+        attr_t target = update_info.t.attributes[i];
+        for (iter=update_info.updateItems.begin(); iter!=update_info.updateItems.end();
+            iter++)
+        {
+            if (target.name == iter->first)                      
+            {
+                string temp = iter->second;                      
+                output.write((char *)temp.c_str(), target.length);
+                break;
+            }
+
+        }
+
+    }
+
+    
 }
 
 
@@ -69,5 +125,6 @@ void Record::Print(record_t * record)
 
 void Record::PrintHead(table_t & table)
 {
+    
 
 }
--- src/Record.h
+++ src/Record.h
@@ -2,17 +2,11 @@
  * Record.h
  */
 
+#ifndef RECORD_H
+#define RECORD_H
 
-#include<iostream>
-using namespace std;
+#include "MiniSQL.h"
 
-typedef struct 
-{   
-    attrtype_t type;
-    string str_value;
-    int int_value;
-    record_t *next;
-} record_t;
 
 class Record
 {
@@ -22,8 +16,10 @@ public:
     int Insert(info_t & insert_info);
     void Delete(info_t & delete_info, index_node_t & index);
     void Update(info_t & update_info, index_node_t & index);
-    record_t *Select(info_t & select_info);
-    void Print(record_t *record);
+    record_t Select(info_t & select_info);
+    void Print(record_t *record);                                 //输出一条记录
     void PrintHead(table_t & table);
 
-}
+};
+
+#endif
